6cd34e2cf856523bc1cbadb0c9a4fe823c6c7bef
codeamac15 <codeamac15@gmail.com>
2016-09-28 15:40:23 -0500

    Primera version del ejercicio de elefantes

---
+++ elefante.rb
@@ -0,0 +1,9 @@
+def elephant
+elephant_counter = 1
+  99.times do 
+   p "#{elephant_counter} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante."
+    elephant_counter += 1 
+  end 
+end  
+
+elephant
